6738d154fdb0ac0b15cb3857a51f915bf393ed40
Canty <chri38eb@edu.zealand.dk>
2023-03-19 14:04:30 +0100

    Input Validation working fior CreateItems

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,9 +1,21 @@
-﻿namespace Items.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Items.Models
 {
     public class Item
     {
+        [Display(Name = "Item Id")]
+        [Required(ErrorMessage = "Der skal angives et ID til Item")]
+        [Range(typeof(int), minimum:"0", maximum:"10000", ErrorMessage = "Id skal være mellem {1} og {2}")]
         public int Id { get; set; }
+
+        [Display(Name = "Navn")]
+        [Required(ErrorMessage = "Der skal angives et Navn til Item"), MaxLength(20)]
         public string Name { get; set; }
+       
+        [Display(Name = "Pris")]
+        [Required(ErrorMessage = "Der skal angives et Pris til Item")]
+        [Range(typeof(double), minimum: "0", maximum: "100000", ErrorMessage = "Prisen skal være mellem {1} og {2}")]
         public double Price { get; set; }
 
         public Item()
