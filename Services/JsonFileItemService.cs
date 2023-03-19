5d8a7eb0cebcdc6991809c482932e87c04b8b4b5
Canty <chri38eb@edu.zealand.dk>
2023-03-19 16:56:03 +0100

    Json working

---
+++ Services/JsonFileItemService.cs
@@ -0,0 +1,41 @@
+﻿using Items.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Text.Json;
+
+namespace Items.Services
+{
+    public class JsonFileItemService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "Items.json"); }
+        }
+
+        public JsonFileItemService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public void SaveJsonItems(List<Item> items)
+        {
+            using (FileStream jsonFileWriter = File.Create(JsonFileName))
+            {
+                Utf8JsonWriter jsonWriter = new Utf8JsonWriter(jsonFileWriter, new JsonWriterOptions()
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+                JsonSerializer.Serialize<Item[]>(jsonWriter, items.ToArray());
+            }
+        }
+
+        public IEnumerable<Item> GetJsonItems()
+        {
+            using (StreamReader jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Item[]>(jsonFileReader.ReadToEnd());
+            }
+        }
+    }
+}
